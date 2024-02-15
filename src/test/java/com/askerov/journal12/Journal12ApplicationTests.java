a6de5add92eebd44d1e532ad39d631e42fe608cd
Samir Askerov <alexsandr.arshavin2003@gmail.com>
2024-02-16 00:01:26 +0300

    Lab1 Аскеров Самир

---
+++ src/test/java/com/askerov/journal12/Journal12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askerov.journal12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Journal12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
